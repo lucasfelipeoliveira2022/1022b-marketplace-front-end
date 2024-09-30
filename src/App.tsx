20b52726fe98f2e2219e0e664bbf54513e246c14
lucasfelipeoliveira2022 <118355862+lucasfelipeoliveira2022@users.noreply.github.com>
2024-09-30 07:08:22 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Lucas Felipe de Oliveira Lima</h1>
+      <h1>Pornhub</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
