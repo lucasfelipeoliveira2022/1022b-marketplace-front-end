6ef38bf1cfc8b8340a4b736910606eb8745ea5ac
lucasfelipeoliveira2022 <118355862+lucasfelipeoliveira2022@users.noreply.github.com>
2024-10-22 07:07:19 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,72 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-type ProdutoType= {
+type ProdutoType = {
   id:number,
-  nome:string
+  nome:string,
   descricao:string,
   preco:string,
   imagem:string
 }
+type UsuarioType = {
+  id:number,
+  nome:string,
+  email:string,
+  created_at:string,
+  updated_at:string 
+}
 function App() {
-  const [produtos, setProdutos] = useState<ProdutoType[]>([])
- // useEffect(oq fazer, qnd fazer)
- useEffect(()=>{
-  console.log("Lucas Felipe")
-  fetch("https://one022b-marketplace-wotq.onrender.com/produtos")
-  .then(resposta=>resposta.json())
-  .then(dados=>setProdutos(dados))
- },[])
+  const [produtos, setProdutos] = useState<ProdutoType[]> ([])
+  const [usuarios, setUsuarios] = useState<UsuarioType[]> ([])
+
+//useEffect(oque fazer e quando fazer)
+  useEffect(()=>{
+    fetch("https://one022b-marketplace-6ac4.onrender.com/produtos")
+    .then(resposta=>resposta.json())
+    .then(dados=>setProdutos(dados))
+  },[])
+  useEffect(()=>{
+    fetch("https://one022b-marketplace-6ac4.onrender.com/usuarios")
+    .then(resposta=>resposta.json())
+    .then(dados=>setUsuarios(dados))
+  },[])
+
+  
   return (
     <>
-      <div className="container-produtos">
-        
-      {produtos.map(prod=>{
-        return(
-          <div key={prod.id} className='produto-item'>
-            <h1>{prod.nome}</h1>
-            <img src={prod.imagem} alt="Imagem do celular"/>
-            <p>{prod.preco}</p>
-            <p>{prod.descricao}</p>
-          </div>
-        )
-      })}
-      </div>
+    <div className='container-produtos'>
+      {
+        produtos.map(prod=>{
+          return(
+            <div key={prod.id} className='produto-item'>
+              <h1>
+                {prod.nome}
+              </h1>
+              <p>{prod.imagem}</p>
+              <p>{prod.preco}</p>
+              <p>{prod.descricao}</p>
+            </div>
+          )
+        })
+      }
+       </div>
+       <div className='container-usuarios'>
+      {
+        usuarios.map(prod=>{
+          return(
+            <div key={prod.id} className='usuario-item'>
+              <h1>
+                {prod.nome}
+              </h1>
+              <p>{prod.email}</p>
+              <p>{prod.created_at}</p>
+              <p>{prod.updated_at}</p>
+            </div>
+          )
+        })
+      }
+       </div>
+       
     </>
   )
 }
